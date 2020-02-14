43e5ae20079846976be5f28ac549b6bee73ac8c1
Vlad-Ber <vladik@live.se>
2020-02-14 13:43:23 +0100

    gammalt

--- 1md034_students-master/app.js
+++ 1md034_students-master/app.js
@@ -70,6 +70,6 @@ io.on('connection', function(socket) {
 });
 
 /* eslint-disable-next-line no-unused-vars */
-const server = http.listen(80,'130.242.99.191', function() {
+const server = http.listen(3000, function() {
   console.log('Server listening on port ' + app.get('port'));
 });
