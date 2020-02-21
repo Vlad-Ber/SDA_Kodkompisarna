cfe44497f75b414eddbf766c4c1bc2224a044302
barwepaul <abarwepaul@gmail.com>
2020-02-21 13:43:28 +0100

    Working hard

---
+++ 1md034_students-master/public/js/profile.js
@@ -0,0 +1,14 @@
+const vm = new Vue({
+	el: '#Profile',
+	data: {
+		name: "Pelle Andersson",
+		age: "45",
+		description: "Här är en beskrivning av personen. Editing-delen ska inte påverkas av hur lång denna är.",
+		img_url: 'img/basicUser.jpg',
+		gender: "Man",
+	},
+
+
+});
+
+
